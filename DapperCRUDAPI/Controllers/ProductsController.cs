2384bdfd9bba13d457241d47bf799699727d1229
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-05-23 19:18:54 -0400

    ProductsController Created and Finalized

---
+++ DapperCRUDAPI/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using DapperCRUDAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DapperCRUDAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductRepository _repository;
+        public ProductsController()
+        {
+            _repository = new ProductRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _repository.GetAllProducts();
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProjectById(int id)
+        {
+            return _repository.GetProductById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.CreateProduct(prod);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateProject(int id, [FromBody] Product prod)
+        {
+            prod.ProductId = id;
+            if (ModelState.IsValid)
+            {
+                _repository.UpdateProject(prod);
+            }
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _repository.DeleteProject(id);
+        }
+    }
+}
--- DapperCRUDAPI/Model/ProductRepository.cs
+++ DapperCRUDAPI/Model/ProductRepository.cs
@@ -1,10 +1,8 @@
 ﻿using Dapper;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DapperCRUDAPI.Model
 {
@@ -13,7 +11,7 @@ namespace DapperCRUDAPI.Model
         private string connectionString;
         public ProductRepository()
         {
-            connectionString = @"Persist Security Info=False;User ID=sa;password=123;Initial Catalog=DAPPERDB; Data Source=DESKTOP-9J6501O\SQLEXPRESS;Connection Timeout=100000;";
+            connectionString = @"Data Source=DESKTOP-9J6501O\SQLEXPRESS;Initial Catalog=DapperCrudDb;Integrated Security=True";
         }
 
         public IDbConnection Connection
